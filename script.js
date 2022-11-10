fe694f47b99a2b221c58dd90854e593484701231
Andrew Connally <ajconnally93@gmail.com>
2022-11-10 12:30:56 -0600

    test commit - untracked

---
+++ script.js
@@ -0,0 +1,4 @@
+const currentDate = moment().format('dddd') + ' ' + moment().format('Do MMM YYYY');
+const currentHour = moment().format('h:mm:ss a');
+
+// Creating for loop to create multiple variables for each text hour
