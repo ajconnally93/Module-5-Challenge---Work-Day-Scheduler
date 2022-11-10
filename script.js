254bc9698a9bd6cd731d58e2d6118e16315c759b
Andrew Connally <ajconnally93@gmail.com>
2022-11-10 13:41:37 -0600

    used moment to have current time displayed to the second. added console logs to test code. used setInterval to accomplish current time displayed

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 const currentDate = moment().format('dddd') + ' ' + moment().format('Do MMM YYYY');
-const currentHour = moment().format('h:mm:ss a');
+const currentTime = moment().format('h:mm:ss a');
 
+// Console logs work and show current day + current time
+console.log(currentDate);
+console.log(currentTime);
+
+//////
 // Creating for loop to create multiple variables for each text hour
+//////
+
+const hour = moment().hours();
+
+// logs current hour without minute or seconds
+console.log(hour);
+
+// function to pass through setInterval to display the live time
+function inInterval() {
+    const currentMoment = moment();
+    $('#currentDay').html(currentMoment.format('YYYY MMMM DD') + ' ' + currentMoment.format('dddd').toUpperCase());
+    $('#currentDay').html(currentDate + ' ' + currentMoment.format('hh:mm:ss A'));
+}
+
+// Now displays the live time by the second (assuming you open with Live Server)
+const interval = setInterval(inInterval, 100);
